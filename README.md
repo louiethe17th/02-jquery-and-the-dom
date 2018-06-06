1341568d9949d632818d7a1fde2b484b9a919590
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-06-06 13:27:25 -0700

    Complete.

--- README.md
+++ README.md
@@ -73,20 +73,23 @@ _Your README.md must include:_
 ```md
 # Project Name
 
-**Author**: Your Name Goes Here
-**Version**: 1.0.0 (increment the patch/fix version number up if you make more commits past your first submission)
+**Author**: Tyler Pearson && Nicklas DiPietro
+**Version**: 1.0.0 
 
 ## Overview
-<!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for a Code Fellows 301 class. (i.e. What's your problem domain?) -->
+This is a blog that allows user to make posts and have them organized based on date posted
 
 ## Getting Started
-<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+Wireframe HTML
+Create Constructor to build blog posts
+build out JS app to append new posts
+Style in css
 
 ## Architecture
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+Built using HTML, CSS, JS and Jquery.
 
 ## Change Log
-<!-- Use this are to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
+Got JS app that appends new posts working
 
 01-01-2001 4:59pm - Application now has a fully-functional express server, with GET and POST routes for the book resource.
 
--- tyler-pearson/scripts/article.js
+++ tyler-pearson/scripts/article.js
@@ -3,19 +3,28 @@
 let articles = [];
 
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// It's a constructor function. rawDataObj represents the information that we will be passing into the constructor function of Article.
 
 function Article (rawDataObj) {
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category
+  this.author = rawDataObj.author
+  this.authorUrl = rawDataObj.authorUrl
+  this.publishedOn =  rawDataObj.publishedOn
+  this.body = rawDataObj.body;
+  
 }
 
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+  // We are doing that to create multiple articles with blank information ready to be inserted with the proper information we are giving it later on.
 
   let $newArticle = $('article.template').clone();
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  
+  $newArticle.removeClass('template');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
@@ -27,6 +36,12 @@ Article.prototype.toHtml = function() {
       3. article title,
       4. article body, and
       5. publication date. */
+
+      $newArticle.find("h1").html(this.title);
+      $newArticle.find(".article-body").html(this.body);
+      $newArticle.find("a").text(this.author);
+      $newArticle.find("a").attr("href", this.authorUrl);
+
 
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -41,10 +56,20 @@ rawData.sort(function(a,b) {
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+// for(let i = 0; i < rawData.length; i++) {
+//   articles.push(new Article(rawData[i]));
+// }
 
-for(let i = 0; i < articles.length; i++) {
-  $('#articles').append(articles[i].toHtml());
-}
+rawData.forEach(function(data){
+
+  articles.push(new Article(data));
+})
+
+// for(let i = 0; i < articles.length; i++) {
+//   $('#articles').append(articles[i].toHtml());
+// }
+
+articles.forEach(function(art){
+  $('#articles').append(art.toHtml())
+
+});
